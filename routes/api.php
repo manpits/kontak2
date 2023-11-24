fc798675441bfa2737eaa374c37a65b731381003
manpits <manpits@gmail.com>
2023-11-24 15:03:34 +0800

    test back kontak with-auth

--- routes/api.php
+++ routes/api.php
@@ -21,12 +21,12 @@ Route::get('/', function () {
 });
 Route::post('/register',[UserController::class, 'register'])->name('api.register');
 Route::post('/login', [UserController::class, 'login'])->name('api.logout');;
-Route::apiResource('/kontak',KontakApiController::class);
 //
 Route::group(['middleware'=>['auth.jwt']],function () {
     Route::get('/user', [UserController::class, 'getUser'])->name('api.getuser');;
     Route::post('/logout',[UserController::class, 'logout'])->name('api.logout');;        
     //
+    Route::apiResource('/kontak',KontakApiController::class);
     Route::get('/provinsi',[WilayahApiController::class,'getProvinsi'])->name('api.provinsi');
     Route::get('/kabupaten/{prov_id}',[WilayahApiController::class,'getKabupatan'])->name('api.kabupaten');
     Route::get('/kecamatan/{kab_id}',[WilayahApiController::class,'getKecamatan'])->name('api.kecamatan');
