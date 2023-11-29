83043df90691655e1dd01c0017107d6559b309f5
manpits <manpits@gmail.com>
2023-11-29 10:42:37 +0800

    user data

--- app/Http/Controllers/KontakApiController.php
+++ app/Http/Controllers/KontakApiController.php
@@ -20,7 +20,7 @@ class KontakApiController extends Controller
      */
     public function index(Request $request)
     {
-        $user = auth('api')->user()->select(['id','nama'])->get();
+        $user = auth('api')->user();
         //
         return response()->json([
             'code'     => 200,
